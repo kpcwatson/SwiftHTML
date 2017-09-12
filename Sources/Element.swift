d01a5bafd964592c0740a7f0da2e6eeedd5e0178
Kyle Watson <kyle.watson@turner.com>
2017-09-12 01:15:35 -0400

    add new lines to tags

--- Sources/Element.swift
+++ Sources/Element.swift
@@ -17,9 +17,9 @@ public struct Element: CustomStringConvertible {
         let openTagWithAttrs = "<\(name)"
             + (attributes.isEmpty ? "" : " ")
             + attributes.map { String(describing: $0) }.joined(separator: " ")
-            + (children == nil ? "" : ">")
+            + (children == nil ? "" : ">\n")
         let nestedChildren = (children ?? []).map { String(describing: $0) }.joined()
-        let closeTag = children == nil ? "/>" : "</\(name)>"
+        let closeTag = children == nil ? "/>\n" : "</\(name)>\n"
         
         return openTagWithAttrs + nestedChildren + closeTag
     }
